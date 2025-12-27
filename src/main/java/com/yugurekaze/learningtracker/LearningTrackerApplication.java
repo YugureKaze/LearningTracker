3bdfe95f6a402424f7961d6c143b7f2f9a910cc9
Yotsume <dreamsartem@gmail.com>
2025-12-27 14:39:19 +0300

    init commit

---
+++ src/main/java/com/yugurekaze/learningtracker/LearningTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.yugurekaze.learningtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningTrackerApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(LearningTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yugurekaze/learningtracker/LearningTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yugurekaze.learningtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
