4ce18a267834320796bb2923fb68600efd055156
Yotsume <dreamsartem@gmail.com>
2025-12-30 19:09:45 +0300

    Add User entity and DB migration

---
+++ src/main/java/com/yugurekaze/learningtracker/user/User.java
@@ -0,0 +1,41 @@
+package com.yugurekaze.learningtracker.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+//TODO add builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence_generator"
+    )
+    @SequenceGenerator(name = "user_sequence_generator",
+            sequenceName = "users_id_seq",
+            initialValue = 1,
+            allocationSize = 1
+    )
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(name = "email", unique = true, length = 255, nullable = false)
+    @EqualsAndHashCode.Include
+    private String email;
+
+    @EqualsAndHashCode.Exclude
+    @Column(name = "password", length = 255, nullable = false)
+    private String password;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
