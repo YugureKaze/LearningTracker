3a9a0d983bef149889c41ff9bd61e56a2aff374c
Yotsume <dreamsartem@gmail.com>
2025-12-31 00:06:04 +0300

    Enable JPA repositories and transaction management

--- src/main/java/com/yugurekaze/learningtracker/LearningTrackerApplication.java
+++ src/main/java/com/yugurekaze/learningtracker/LearningTrackerApplication.java
@@ -3,12 +3,16 @@ package com.yugurekaze.learningtracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableJpaRepositories
 @EnableJpaAuditing
 public class LearningTrackerApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(LearningTrackerApplication.class, args);
     }
 
