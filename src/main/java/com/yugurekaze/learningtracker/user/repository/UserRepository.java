26ec4e0e62e99b6ea264cde5979fd6d065b6f657
Yotsume <dreamsartem@gmail.com>
2025-12-31 00:06:33 +0300

    Refactor UserRepository: remove @NonNull, add password update

--- src/main/java/com/yugurekaze/learningtracker/user/repository/UserRepository.java
+++ src/main/java/com/yugurekaze/learningtracker/user/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.yugurekaze.learningtracker.user.repository;
 import com.yugurekaze.learningtracker.user.model.User;
-import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -9,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<@NonNull User,@NonNull Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 
@@ -17,5 +16,9 @@ public interface UserRepository extends JpaRepository<@NonNull User,@NonNull Lon
     @Query("UPDATE User u SET u.email = :newEmail WHERE u.id = :id")
     //TODO fix this method, for example with user password
     void changeUserEmail(@Param("id") Long id, @Param("newEmail") String newEmail);
+
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE User u SET u.password = :newPassword WHERE u.id = :id")
+    void changeUserPassword(@Param("id") Long id, @Param("newPassword") String newPassword);
 
 }
