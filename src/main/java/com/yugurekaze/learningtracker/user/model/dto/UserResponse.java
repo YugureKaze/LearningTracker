519c4147eab06022ac6218d1a801aca0d62072a0
Yotsume <dreamsartem@gmail.com>
2025-12-31 00:06:24 +0300

    Remove validation annotations from UserResponse fields

--- src/main/java/com/yugurekaze/learningtracker/user/model/dto/UserResponse.java
+++ src/main/java/com/yugurekaze/learningtracker/user/model/dto/UserResponse.java
@@ -1,19 +1,10 @@
 package com.yugurekaze.learningtracker.user.model.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-
 import java.time.LocalDateTime;
 
 public record UserResponse(
-        @NotBlank
-        @Email(regexp = "^[A-Za-z0-9+_.-]+@(.+)$")
-        @Size(max = 255)
-        String email,
 
-        @NotNull
+        String email,
         LocalDateTime createdAt
 ) {
 }
